e4d0f0b80f196896f8b32d1514b094af31bc6715
jameswsullivan <81947235+jameswsullivan@users.noreply.github.com>
2024-03-06 15:37:57 -0600

    Update bash commands

--- Bash/General_Script_Commands.sh
+++ Bash/General_Script_Commands.sh
@@ -1,4 +1,16 @@
-# Copy file to Ubuntu Server using SCP
-scp <source file> <username@server>:<destination file path>
+# scp
+scp -r <source> <destination>
+scp -r <username@server>:<source file path> <username@server>:<destination file path>
 
-# Load *.sql.gz file into MySQL database using pv.
+# ln
+ln -s target_file symbolic_link
+
+# ls
+ls -alh
+ls -A1
+
+# awk
+ls -A1 | awk '{print "scp " $0 " user@server:/remote/directory/"}'
+ls -A1 | awk '{print "scp -r user@server:/remote/directory/" $0 "/*" " /destination/directory/" $0} END {print ""}' | tee /output-file.txt
+ls -A1 | awk '{print "cp -a /source/directory/" $0 " /destination/directory/"} END {print ""}' | tee /output-file.txt
+ls -A1 | awk '{print "du -sh " $0}'
