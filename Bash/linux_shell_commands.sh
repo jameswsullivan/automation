f7816be6d72fe6558509eb3e8b1ab3c8e6e130b1
James Sullivan <81947235+jameswsullivan@users.noreply.github.com>
2024-10-17 22:25:51 -0500

    Update and rename General_Script_Commands.sh to linux_shell_commands.sh

--- Bash/linux_shell_commands.sh
+++ Bash/linux_shell_commands.sh
@@ -14,3 +14,12 @@ ls -A1 | awk '{print "scp " $0 " user@server:/remote/directory/"}'
 ls -A1 | awk '{print "scp -r user@server:/remote/directory/" $0 "/*" " /destination/directory/" $0} END {print ""}' | tee /output-file.txt
 ls -A1 | awk '{print "cp -a /source/directory/" $0 " /destination/directory/"} END {print ""}' | tee /output-file.txt
 ls -A1 | awk '{print "du -sh " $0}'
+
+# df
+df -h --output=source,size,used,avail,pcent
+
+# find :
+find / -name <STUFF_TO_FIND>
+
+# history :
+history -c && history -w
