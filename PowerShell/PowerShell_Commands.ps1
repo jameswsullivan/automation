685db2ee585a3e36c259fe09c8094a9d2f6c2eba
James Sullivan <81947235+jameswsullivan@users.noreply.github.com>
2025-04-11 18:50:36 -0500

    Update PowerShell_Commands.ps1

--- PowerShell/PowerShell_Commands.ps1
+++ PowerShell/PowerShell_Commands.ps1
@@ -1,3 +1,28 @@
+# Install winget:
+
+Find-PackageProvider -Name "NuGet" -AllVersions
+Install-PackageProvider -Name "NuGet" -RequiredVersion "2.8.5.216" -Force
+
+$progressPreference = 'silentlyContinue'
+Write-Host "Installing WinGet PowerShell module from PSGallery..."
+Install-PackageProvider -Name NuGet -Force | Out-Null
+Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
+Write-Host "Using Repair-WinGetPackageManager cmdlet to bootstrap WinGet..."
+Repair-WinGetPackageManager
+Write-Host "Done."
+
+# Install and show PowerShell Version
+$PSVersionTable
+winget search Microsoft.PowerShell
+winget install --id Microsoft.Powershell --source winget
+
+# Tweak PowerShell Console Colors
+$host.privatedata
+$host.privatedata.ErrorForegroundColor = 'White'
+
+Console Color Reference
+https://learn.microsoft.com/en-us/dotnet/api/system.consolecolor?view=net-7.0
+
 # Configure Storage Pool and Virtual Disk using PowerShell.
 New-StoragePool
 
@@ -10,16 +35,8 @@ Get-VirtualDisk
 Get-StoragePool -FriendlyName <STORAGE_POOL_NAME>
 Get-PhysicalDisk
 
-
 # Get PowerShell command history.
 Get-History
-
-
-# Show PowerShell Version
-$PSVersionTable
-winget search Microsoft.PowerShell
-winget install --id Microsoft.Powershell --source winget
-
 
 # Install/Remove PowerShell Modules
 Install-Module
@@ -28,15 +45,12 @@ Remove-Module
 e.g.
 Install-Module -Name SharePointPnPPowerShell2019 -RequiredVersion 3.29.2101.0
 
-
 # Add SharePoint PowerShell Snapin
 Add-PSSnapin Microsoft.SharePoint.PowerShell
-
 
 # Get SharePoint Instance
 Get-SPServiceInstance | Where-Object {$_.TypeName -eq 'Central Administration'}
 Get-SPServer
-
 
 # Display full width text pipe result into:
 Format-Table -AutoSize
@@ -44,18 +58,8 @@ Format-Table -AutoSize
 e.g.
 Get-PnPGroup | Format-Table -AutoSize | Tee-Object AllGroupsOnHub.log
 
-
-# Tweak PowerShell Console Colors
-$host.privatedata
-$host.privatedata.ErrorForegroundColor = 'White'
-
-Console Color Reference
-https://learn.microsoft.com/en-us/dotnet/api/system.consolecolor?view=net-7.0
-
-
 # Change PowerShell ISE Tab Name
 $psise.CurrentPowerShellTab.DisplayName = ‘Docker Host’
-
 
 # Connect to remote computer via PowerShell and add remote computer to TrustedHosts
 # On the computer you're currently working ON, not the remote computer you're remoting TO.
@@ -64,10 +68,8 @@ Get-Item -Path WSMan:\localhost\Client\TrustedHosts
 Set-Item WSMan:\localhost\Client\TrustedHosts -Value "IP_of_Remote_Machine"
 Enter-PSSession -ComputerName "IP_ADDR or PC_NAME" -Credential "Username"
 
-
 # Test whether a port is open on a server with PowerShell
 Test-NetConnection -ComputerName 192.168.1.1 -Port 443
-
 
 # Set-ExecutionPolicy
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
